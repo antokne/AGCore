e7fc91f249b5854e7ace87d61c4e99408e3cb92a
Ants Gardiner <antokne@gmail.com>
2023-02-16 10:32:36 +1300

    First commit

---
+++ Sources/AGCore/Foundation/FileManager+Extensions.swift
@@ -0,0 +1,31 @@
+//
+//  FileManager+Extensions.swift
+//  
+//
+//  Created by Antony Gardiner on 16/02/23.
+//
+
+import Foundation
+
+public struct FileManagerHelpers {
+	
+	
+	
+	public func fileSize(url: URL) -> Int? {
+		
+		if #available(macOS 13.0, *) {
+			if let attributes = try? FileManager.default.attributesOfItem(atPath: url.path(percentEncoded: false)) {
+				let size = attributes[FileAttributeKey.size]
+				return size as? Int
+			}
+		}
+		
+		return nil
+	}
+}
+
+extension FileManager {
+	
+	public static var helpers: FileManagerHelpers = FileManagerHelpers()
+	
+}
