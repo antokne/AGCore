7938a537585da2a9eb4857c39bafdf9743b441af
Ants Gardiner <antokne@gmail.com>
2023-05-09 09:46:42 +1200

    Added UInt8 masking and bit extracting.

---
+++ Sources/AGCore/Foundation/Integer+Extensions.swift
@@ -0,0 +1,30 @@
+//
+//  Integer+Extensions.swift
+//  
+//
+//  Created by Antony Gardiner on 8/05/23.
+//
+
+import Foundation
+
+
+extension UInt8 {
+	
+	/// Calculates the bit mask for extracting x bits from a byte
+	/// - Parameter bits: number of bits to extract 0-7
+	/// - Returns: the binary mask to extract those bits
+	public static func mask(for bits: UInt8) -> UInt8 {
+		
+		let fullMask = UInt8(0b11111111)
+		
+		guard bits >= 0 && bits <= 7 else {
+			return fullMask
+		}
+		
+		return fullMask >> (7 - bits)
+	}
+	
+	public func extractBits(using mask: UInt8) -> UInt8 {
+		self & mask
+	}
+}
