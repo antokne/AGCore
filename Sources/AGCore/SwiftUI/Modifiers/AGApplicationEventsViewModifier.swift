3d6ff1ff542ef43f2d7161371c26275b26f74d62
Ant Gardiner <antokne@gmail.com>
2023-07-04 17:06:45 +1200

    Added view modifier for will enter foreground event.

---
+++ Sources/AGCore/SwiftUI/Modifiers/AGApplicationEventsViewModifier.swift
@@ -0,0 +1,31 @@
+//
+//  File.swift
+//  
+//
+//  Created by Ant Gardiner on 4/07/23.
+//
+
+import SwiftUI
+
+#if os(iOS)
+public struct AGApplicationEventsViewModifier: ViewModifier {
+    let action: () -> Void
+    
+    public func body(content: Content) -> some View {
+        content
+            .onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification)) { _ in
+                action()
+            }
+    }
+}
+
+extension View {
+    
+    /// Notification sent when application will enter the foreground
+    /// - Parameter action: action to call when event occurs.
+    /// - Returns: A view that triggers on event
+    public func willEnterForeground(perform action: @escaping () -> Void) -> some View {
+        self.modifier(AGApplicationEventsViewModifier(action: action))
+    }
+}
+#endif
