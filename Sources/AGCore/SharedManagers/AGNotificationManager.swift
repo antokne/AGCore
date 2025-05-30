d166ad0ec2f7a517669a672286c4f54352903ee5
Ant Gardiner <antokne@gmail.com>
2025-05-31 09:20:27 +1200

    Moving notifications into shared module, a wip.

--- Sources/AGCore/SharedManagers/AGNotificationManager.swift
+++ Sources/AGCore/SharedManagers/AGNotificationManager.swift
@@ -7,12 +7,10 @@
 
 import Foundation
 import UserNotifications
-
+import NiceNotifications
 
 public class AGNotificationManager {
 	
-	public static let shared: AGNotificationManager = AGNotificationManager()
-
 	private var notificationDelegate = AGNotificationDelegate()
 
 	public init() {
@@ -26,11 +24,49 @@ public class AGNotificationManager {
 	public func setPresentationOption(options: [String: UNNotificationPresentationOptions]) {
 		notificationDelegate.presentationOptions = options
 	}
+
+	public func postNotificationNow(
+		title: String,
+		message: String,
+		sound: UNNotificationSound = .default,
+		id: String
+	) {
+		let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
+		scheduleNotification(title: title, message: message, trigger: trigger, id: id)
+	}
+	
+	public func scheduleNotification(
+		title: String,
+		message: String,
+		trigger: UNNotificationTrigger,
+		sound: UNNotificationSound = .default,
+		id: String
+	) {
+		let content = UNMutableNotificationContent()
+		content.title = title
+		content.body = message
+		content.sound = sound
+		let request = UNNotificationRequest(identifier: id, content: content, trigger: trigger)
+		LocalNotifications.directSchedule(request: request, permissionStrategy: .scheduleIfSystemAllowed)
+	}
+	
+	public func removeNotification(id: String) {
+		LocalNotifications.Env.removePendingNotificationRequests([id])
+	}
+	
+	public func getCurrentPendingNotifications() async -> [UNNotificationRequest] {
+		await UNUserNotificationCenter.current().pendingNotificationRequests()
+	}
+	
 }
 
 
 private class AGNotificationDelegate: NSObject {
 	var presentationOptions: [String: UNNotificationPresentationOptions] = [: ]
+	
+	func receivedNotification(info: [AnyHashable : Any]) {
+		
+	}
 }
 
 extension AGNotificationDelegate: UNUserNotificationCenterDelegate {
@@ -47,6 +83,8 @@ extension AGNotificationDelegate: UNUserNotificationCenterDelegate {
 	}
 	
 	func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+		let userInfo = response.notification.request.content.userInfo
+		receivedNotification(info: userInfo)
 		completionHandler()
 	}
 }
